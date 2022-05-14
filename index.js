d8ee0c93ae5fb419f2a62b0548eeec74cbd62559
tzada8 <tzada@uwaterloo.ca>
2022-05-13 23:48:17 -0400

    feat(Bing Search): Created endpoint that GETs data from bing search API

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const http = require('http');
+const bingSearch = require("./src/bing-search");
+
+const query = "Distance between California and Toronto?"
+const search = bingSearch.search(query);
 
 const server = http.createServer((req, res) => {
-        res.end('This is my server response!');
+  search.then(response => console.log(response));
+  res.end('This is my server response!');
 });
 
 server.listen(process.env.PORT || 3000);
-
---
+++ src/bing-search.js
@@ -0,0 +1,29 @@
+const axios = require("axios");
+
+const options = {
+  method: 'GET',
+  url: 'https://bing-web-search1.p.rapidapi.com/search',
+  params: {
+    q: '', // Updated depending on params.
+    mkt: 'en-us',
+    safeSearch: 'Off',
+    textFormat: 'Raw',
+    freshness: 'Day'
+  },
+  headers: {
+    'X-BingApis-SDK': 'true',
+    'X-RapidAPI-Host': 'bing-web-search1.p.rapidapi.com',
+    'X-RapidAPI-Key': 'cfdf27182dmshbf97c52786708a7p1d6790jsn4b62b203b61a'
+  }
+};
+
+exports.search = async (q) => {
+  options.params.q = q;
+  try {
+    const response = await axios.request(options);
+    const data = response.data.webPages;
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
