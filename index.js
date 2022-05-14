ab280d773c22f2d3c4335a1f87b5697eef466bf1
Geordy Decena <geordydecena@MacBook-Pro-3.local>
2022-05-14 00:02:40 -0400

    initial twilio setup

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const http = require('http');
+const express = require('express');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const bodyParser = require('body-parser');
 
-const server = http.createServer((req, res) => {
-        res.end('This is my server response!');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/sms', (req, res) => {
+        const twiml = new MessagingResponse();
+        twiml.message(req.body.Body);
+
+        res.writeHead(200, { 'Content-Type': 'text/xml' });
+        res.end(twiml.toString());
 });
 
-server.listen(process.env.PORT || 3000);
-
+http.createServer(app).listen(1337, () => {
+        console.log('Express server listening on port 1337');
+});
